268081f66198a0c31252d2454857450b0dca3471
Jaras49 <jaras48@gmail.com>
2018-05-23 15:43:38 +0200

    Added classes that implements Check interface

--- src/main/java/com/sudoku/Solver/check/Check.java
+++ src/main/java/com/sudoku/Solver/check/Check.java
@@ -1,6 +1,9 @@
 package com.sudoku.Solver.check;
 
+import com.sudoku.board.elements.SudokuBoard;
+
 public interface Check {
 
+    SudokuBoard check(SudokuBoard board);
 
 }
---
+++ src/main/java/com/sudoku/Solver/check/ColumnCheck.java
@@ -0,0 +1,11 @@
+package com.sudoku.Solver.check;
+
+import com.sudoku.board.elements.SudokuBoard;
+
+public class ColumnCheck implements Check {
+
+    @Override
+    public SudokuBoard check(SudokuBoard board) {
+        return null;
+    }
+}
