0f53c4ab1180aa4aa1e6e59affbb025d7ab61869
Jaras49 <jaras48@gmail.com>
2018-05-23 21:24:19 +0200

    BlockCheck implementation

--- src/main/java/com/sudoku/Solver/check/BlockCheck.java
+++ src/main/java/com/sudoku/Solver/check/BlockCheck.java
@@ -1,11 +1,63 @@
 package com.sudoku.Solver.check;
 
-import com.sudoku.board.elements.SudokuBoard;
 
-public class BlockCheck implements Check {
+import com.sudoku.board.elements.Block;
+import com.sudoku.board.elements.SudokuElement;
+
+import java.util.List;
+import java.util.Set;
+
+public class BlockCheck extends AbstractCheck implements Check {
 
     @Override
-    public SudokuBoard check(SudokuBoard board) {
-        return null;
+    protected boolean checkIfIsTaken(int rowIndex, int elementIndex, int possibleValue) {
+
+        Block blockY = Block.evaluateBlock(rowIndex);
+        Block blockX = Block.evaluateBlock(elementIndex);
+
+        for (int i : blockY.getPossibleValues()) {
+
+            List<SudokuElement> sudokuElements = board.getSudokuRows().get(i).getSudokuElements();
+
+            for (int j : blockX.getPossibleValues()) {
+
+                if (i == rowIndex && j == elementIndex) {
+                    continue;
+                }
+
+                int value = sudokuElements.get(j).getValue();
+
+                if (value == possibleValue) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean checkIfIsAmongPossibleValues(int rowIndex, int elementIndex, int possibleValue) {
+
+        Block blockY = Block.evaluateBlock(rowIndex);
+        Block blockX = Block.evaluateBlock(elementIndex);
+
+        for (int i : blockY.getPossibleValues()) {
+
+            List<SudokuElement> sudokuElements = board.getSudokuRows().get(i).getSudokuElements();
+
+            for (int j : blockX.getPossibleValues()) {
+
+                if (i == rowIndex && j == elementIndex) {
+                    continue;
+                }
+
+                Set<Integer> possibleValues = sudokuElements.get(j).getPossibleValues();
+
+                if (possibleValues.contains(possibleValue)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
---
+++ src/test/java/com/sudoku/Solver/check/BlockCheckTestSuite.java
@@ -0,0 +1,33 @@
+package com.sudoku.Solver.check;
+
+import com.sudoku.Solver.check.exception.InvalidSudokuException;
+import com.sudoku.board.SudokuBoardInitializer;
+import com.sudoku.board.elements.SudokuBoard;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BlockCheckTestSuite {
+
+    @Test
+    public void shouldCheckBlock() throws InvalidSudokuException {
+
+        //Given
+        SudokuBoard board = new SudokuBoardInitializer().createBoard(4);
+        Check check = new BlockCheck();
+
+        board.getSudokuRows().get(0).getSudokuElements().get(0).setValue(5);
+        board.getSudokuRows().get(1).getSudokuElements().get(1).getPossibleValues().clear();
+        board.getSudokuRows().get(1).getSudokuElements().get(1).getPossibleValues().add(5);
+        board.getSudokuRows().get(1).getSudokuElements().get(1).getPossibleValues().add(8);
+
+        //When
+        SudokuBoard solvedBoard = check.check(board, 1, 1);
+        int resultValue = solvedBoard.getSudokuRows().get(1).getSudokuElements().get(1).getValue();
+
+        //Then
+        assertEquals(8, resultValue);
+
+    }
+
+}
